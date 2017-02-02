562bb1fb37caa832fcc7bfe410895f38877b8844
Ali Reza <alirezayahya@gmail.com>
2017-02-02 15:35:36 +0700

    update http-log-tail

--- app.go
+++ app.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os/exec"
 
-	"github.com/hpcloud/tail"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -14,6 +15,31 @@ var (
 	port     = flag.String("port", "9000", "HTTP server port")
 	basePath = flag.String("base_path", "/var/log/", "Log base path")
 )
+
+func tail(path string) (chan []byte, chan error, error) {
+	_, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	errChan := make(chan error)
+	w := newWatcher()
+	go func() {
+		cmds := []string{"tail", "-f", path}
+		fmt.Println("Executing : ", cmds)
+
+		cmd := exec.Command(cmds[0], cmds[1:]...)
+
+		cmd.Stdout = w
+		cmd.Stderr = w
+
+		if errno := cmd.Run(); errno != nil {
+			errChan <- errno
+		}
+	}()
+
+	return w.out, errChan, nil
+}
 
 func main() {
 	flag.Parse()
@@ -29,20 +55,40 @@ func main() {
 		}
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 
-		t, err := tail.TailFile(*basePath+ps.ByName("path"), tail.Config{Follow: true})
-
+		outChan, errChan, err := tail(*basePath + ps.ByName("path"))
 		if err != nil {
-			log.Printf("[ERROR] TailFile : %s", err)
-			http.Error(w, "File not found!", http.StatusNotFound)
+			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}
-
-		for line := range t.Lines {
-			fmt.Fprintf(w, line.Text+"\n")
-			flusher.Flush()
+		for {
+			select {
+			case out := <-outChan:
+				fmt.Fprintf(w, string(out))
+				flusher.Flush()
+			case err := <-errChan:
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				break
+			}
 		}
 	})
 
+	fmt.Printf("Application running on port %s", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, router))
-	log.Printf("Application running on port %s", *port)
+}
+
+type watcher struct {
+	out chan []byte
+}
+
+func newWatcher() *watcher {
+	w := &watcher{
+		out: make(chan []byte),
+	}
+
+	return w
+}
+
+func (c *watcher) Write(b []byte) (int, error) {
+	c.out <- b
+	return len(b), nil
 }
